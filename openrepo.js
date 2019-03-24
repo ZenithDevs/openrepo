9e73624b3239d8027774ee0def1070c6b23f4dbe
Magnetar <magnetardev@icloud.com>
2019-03-24 15:29:52 -0400

    v1.0.1

--- openrepo.js
+++ openrepo.js
@@ -23,120 +23,101 @@
 */
 
 class OpenRepo {
-	constructor(key, defaultItems) {
+	constructor(key, defaultItems, storageHandler) {
 		this.key = key;
 		this.defaultItems = defaultItems;
-		if (localStorage.getItem(this.key) === null) {
-      localStorage.setItem(this.key, (this.defaultItems != null) ? JSON.stringify(this.defaultItems) : JSON.stringify([]));
-    }
+		this.storageHandler = (storageHandler != null) ? storageHandler : localStorage;
+		if (this.storageHandler.getItem(this.key) === null) {
+	      this.storageHandler.setItem(this.key, (this.defaultItems != null) ? JSON.stringify(this.defaultItems) : JSON.stringify([]));
+	    }
 	}
 	add(url) {
-		var repos = JSON.parse(localStorage.getItem(this.key));
-    repos.push(this.patch(url));
-    localStorage.setItem(key, JSON.stringify(repos.clean()));
+		var repos = JSON.parse(this.storageHandler.getItem(this.key));
+	    repos.push(this.patch(url));
+	    this.storageHandler.setItem(this.key, JSON.stringify(this.clean(repos)));
 	}
 	remove(item) {
-    var repos = JSON.parse(localStorage.getItem(this.key));
+	    var repos = JSON.parse(this.storageHandler.getItem(this.key));
 		switch (typeof item) {
 			case "number": 
-        repos.splice(i, 1);
-        localStorage.setItem(this.key, JSON.stringify(repos));
+		        repos.splice(i, 1);
+		        this.storageHandler.setItem(this.key, JSON.stringify(repos));
 				break;
 			case "string":
-				for (var i = 0; i < repos.length; i++) {
-		      if (repos[i] == item) {
-		        repos.splice(i, 1);
-		        localStorage.setItem(this.key, JSON.stringify(repos));
-		      }
-		    }
+				repos.forEach((repo, i) => {
+			      	if (repo == item) {
+				        repos.splice(i, 1);
+				        this.storageHandler.setItem(this.key, JSON.stringify(repos));
+				    }
+				});
 				break;
 			default:
 				break;
 		}
 	}
 	reset() {
-		localStorage.removeItem(this.key);
-		if (localStorage.getItem(this.key) === null) {
-      localStorage.setItem(this.key, (this.defaultItems != null) ? JSON.stringify(this.defaultItems) : JSON.stringify([]));
-    }
+		this.storageHandler.removeItem(this.key);
+		if (this.storageHandler.getItem(this.key) === null) {
+	    	this.storageHandler.setItem(this.key, (this.defaultItems != null) ? JSON.stringify(this.defaultItems) : JSON.stringify([]));
+	    }
 	}
 	get(url) {
 		var self = this;
 		return new Promise(function(resolve, reject) {
-	    var xhr = new XMLHttpRequest();
-	    xhr.open("GET", url);
-	    xhr.onload = function() {
-	      if (this.status >= 200 && this.status < 300) {
-	      	try {
-	      		var data = JSON.parse(xhr.response);
+			fetch(url).then(res => res.json()).then(json => {
 		        resolve(self.patch(data));
-	      	} catch(err) {
-	      		reject({
-		          status: this.status,
-		          statusText: "Unable to parse JSON."
-		        });
-	      	}
-	      } else {
-	        reject({
-	          status: this.status,
-	          statusText: xhr.statusText
-	        });
-	      }
-	    };
-	    xhr.onerror = function() {
-	      reject({
-	        status: this.status,
-	        statusText: xhr.statusText
-	      });
-	    };
-	    xhr.send();
-	  });
-  }
+			}).catch(err => {
+				reject(err);
+			});
+	  	});
+	}
 	list(fetchAll) {
 		var self = this;
-		try {
-			var list = [];
-			var repos = JSON.parse(localStorage.getItem(self.key));	
-			if (fetchAll == true) {
-				return new Promise(function(resolve, reject) {
-					var reposLoaded = 0;
-					repos.forEach((repo) => {
-						self.get(repo).then(function(data) {
-							list.push(data);
-							reposLoaded++;
-							if (reposLoaded == repos.length) {
-								resolve(list);
-							}
-						}).catch(function(err) {
-							console.log(err);
-						});
-					});
-				});
+        var repos = JSON.parse(this.storageHandler.getItem(self.key));
+		return (new Promise((resolve, reject) => {
+			var result = [];
+			var loadedRepos = 0;
+            if (repos.length > 0 && fetchAll == true) {
+              	repos.forEach(repo => {
+	                fetch(repo).then(res => res.json()).then(json => {
+	                	result.push(json);
+	                  	loadedRepos++;
+	                  	if (loadedRepos == repos.length) {
+	                  		resolve(result);
+	                  	}
+	                }).catch(err => {
+	                  	console.error(err);
+	                  	loadedRepos++;
+	                  	if (loadedRepos == repos.length) {
+	                    	resolve(result);
+	                  	}
+                	});
+              	});
 			} else {
-				return new Promise(function(resolve, reject) {
-					resolve(repos);
-				});
-			}
-		} catch (err) {
-			console.error(err);
-		}
+	        	resolve(repos);
+            }
+    	}));
 	}
 	patch(item) {
 		var self = this;
 		switch (typeof item) {
-	    case "string":
-	      return item.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;").replace(/\\/g, "&#92;").replace(/`/g, "&#96;");
-	      break;
-	    case "object":
-	      var object = item;
-	      var keys = Object.keys(object);
-	      for (var i = 0; i < keys.length; i++) {
-	        object[keys[i]] = self.patch(object[keys[i]]);
-	      }
-	      return object;
-	      break;
-	    default:
-	      return item;
-	  }
+		    case "string":
+		    	return item.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;").replace(/\\/g, "&#92;").replace(/`/g, "&#96;");
+		    	break;
+		    case "object":
+		    	var object = item;
+		    	Object.keys(object).forEach(keys => {
+			        object[key] = self.patch(object[key]);
+		    	});
+		      	return object;
+		      	break;
+		    default:
+		    	return item;
+	  	}
+	}
+	clean(arr) {
+		return arr.filter(function(elem, index, self) {
+		    return index === self.indexOf(elem);
+		});
 	}
 }
